c02d2ad8dc6f53c99ede49beca1e0823d90b4db3
AntonMenchaca <menchacanton@gmail.com>
2025-07-13 22:07:42 -0400

    Finish 2.3.12

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -1,4 +1,4 @@
-import { FormGroup, FormLabel, FormControl, FormSelect } from "react-bootstrap";
+import { FormGroup, FormLabel, FormControl, FormSelect, Form } from "react-bootstrap";
 export default function BootstrapForms() {
   return (
     <div>
@@ -23,6 +23,22 @@ export default function BootstrapForms() {
           <option value="3">Three</option>
         </FormSelect>
       </div>
+      <br />
+      <div id="wd-css-styling-switches">
+        <h3>Switches</h3>
+        <Form.Check type="switch" checked={false} id="wd-switch-1"
+          label="Unchecked switch checkbox input" />
+        <Form.Check type="switch" checked={true} id="wd-switch-2"
+          label="Checked switch checkbox input" />
+        <Form.Check type="switch" checked={false} disabled
+          id="custom-switch"
+          label="Unchecked disabled switch checkbox input" />
+        <Form.Check type="switch" checked={true} disabled
+          id="custom-switch"
+          label="Checked disabled switch checkbox input" />
+      </div>
+
+
     </div>
   )
 }
