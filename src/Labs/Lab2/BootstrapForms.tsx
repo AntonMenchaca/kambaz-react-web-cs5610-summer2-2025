7273179e5526aed71914f829bfa9a7508143415e
AntonMenchaca <menchacanton@gmail.com>
2025-07-13 22:07:02 -0400

    Finish 2.3.11

--- src/Labs/Lab2/BootstrapForms.tsx
+++ src/Labs/Lab2/BootstrapForms.tsx
@@ -1,17 +1,28 @@
-import { FormGroup, FormLabel, FormControl } from "react-bootstrap";
+import { FormGroup, FormLabel, FormControl, FormSelect } from "react-bootstrap";
 export default function BootstrapForms() {
   return (
-    <div id="wd-css-styling-forms">
-      <h2>Forms</h2>
-      <FormGroup className="mb-3" controlId="wd-email">
-        <FormLabel>Email address</FormLabel>
-        <FormControl type="email" placeholder="name@example.com" />
-      </FormGroup>
-      <FormGroup className="mb-3" controlId="wd-textarea">
-        <FormLabel>Example textarea</FormLabel>
-        <FormControl as="textarea" rows={3} />
-      </FormGroup>
+    <div>
+      <div id="wd-css-styling-forms">
+        <h2>Forms</h2>
+        <FormGroup className="mb-3" controlId="wd-email">
+          <FormLabel>Email address</FormLabel>
+          <FormControl type="email" placeholder="name@example.com" />
+        </FormGroup>
+        <FormGroup className="mb-3" controlId="wd-textarea">
+          <FormLabel>Example textarea</FormLabel>
+          <FormControl as="textarea" rows={3} />
+        </FormGroup>
+      </div>
+      <br />
+      <div id="wd-css-styling-dropdowns">
+        <h3>Dropdowns</h3>
+        <FormSelect>
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </FormSelect>
+      </div>
     </div>
-
   )
 }
