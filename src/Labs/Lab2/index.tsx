d7e192529ea7a0f1959ef96a34b628df7739f9f8
AntonMenchaca <menchacanton@gmail.com>
2025-07-13 21:22:55 -0400

    Finish 2.3.2

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -12,10 +12,11 @@ import Float from './Float';
 import GridLayout from './GridLayout';
 import Flex from './Flex';
 import ReactIcons from './ReactIcons';
+import { Container } from 'react-bootstrap';
 
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p >
@@ -80,5 +81,5 @@ export default function Lab2() {
       <GridLayout />
       <Flex />
       <ReactIcons />
-    </div>);
+    </Container>);
 }
