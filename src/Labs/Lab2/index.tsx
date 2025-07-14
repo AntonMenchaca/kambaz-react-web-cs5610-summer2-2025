f7a8e6334081ddc6745c4db32e5024b106a65ea9
AntonMenchaca <menchacanton@gmail.com>
2025-07-13 21:17:06 -0400

    Finish 2.1.19

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -10,6 +10,7 @@ import Positions from './Positions';
 import Zindex from './Zindex';
 import Float from './Float';
 import GridLayout from './GridLayout';
+import Flex from './Flex';
 export default function Lab2() {
   return (
     <div id="wd-lab2">
@@ -75,5 +76,6 @@ export default function Lab2() {
       <Zindex />
       <Float />
       <GridLayout />
+      <Flex />
     </div>);
 }
