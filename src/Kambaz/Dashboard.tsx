1778d2c19c1315507a8b03784db7e7b849f9dfb6
AntonMenchaca <menchacanton@gmail.com>
2025-07-15 08:53:27 -0400

    Refactor Kambaz Dashboard, Naigation, and Courses Navigation to use react bootstrap

--- src/Kambaz/Dashboard.tsx
+++ src/Kambaz/Dashboard.tsx
@@ -1,157 +1,119 @@
 import { Link } from "react-router-dom";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import "./styles.css";
+const courses = [
+  {
+    id: "1234",
+    img: "/images/reactjs.jpg",
+    title: "CS1234 React JS",
+    description: "Full Stack software developer",
+  },
+  {
+    id: "5678",
+    img: "/images/intensiveFoundations.png",
+    title: "CS5001 Intensive Foundations of CS",
+    description: "Intensive Foundations of Computer Science",
+  },
+  {
+    id: "91011",
+    img: "/images/discreteStructures.jpg",
+    title: "CS5002 Discrete Structures",
+    description: "Discrete Structures",
+  },
+  {
+    id: "121314",
+    img: "/images/java.png",
+    title: "CS5003 Object Oriented Programming",
+    description: "Object Oriented Programming",
+  },
+  {
+    id: "151617",
+    img: "/images/dataStructures.jpg",
+    title: "CS5004 Data Structures and Algorithms",
+    description: "Data Structures and Algorithms",
+  },
+  {
+    id: "181920",
+    img: "/images/javascript.jpg",
+    title: "CS5005 Software Engineering",
+    description: "Software Engineering",
+  },
+  {
+    id: "212223",
+    img: "/images/htmlcss.jpg",
+    title: "CS5006 Web Development",
+    description: "Web Development",
+  },
+  {
+    id: "242526",
+    img: "/images/mobile.png",
+    title: "CS5007 Mobile App Development",
+    description: "Mobile App Development",
+  },
+  {
+    id: "272829",
+    img: "/images/database.png",
+    title: "CS5008 Database Systems",
+    description: "Database Systems",
+  },
+  {
+    id: "303132",
+    img: "/images/cloud.png",
+    title: "CS5009 Cloud Computing",
+    description: "Cloud Computing",
+  },
+  {
+    id: "333435",
+    img: "/images/machinelearning.png",
+    title: "CS5010 Machine Learning",
+    description: "Machine Learning",
+  },
+  {
+    id: "363738",
+    img: "/images/artificialintelligence.jpeg",
+    title: "CS5011 Artificial Intelligence",
+    description: "Artificial Intelligence",
+  },
+];
 
 export default function Dashboard() {
   return (
-    <div id="wd-dashboard">
-      <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-      <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
-      <div id="wd-dashboard-courses">
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/1234/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/reactjs.jpg" width={200} />
-            <div>
-              <h5> CS1234 React JS </h5>
-              <p className="wd-dashboard-course-title">
-                Full Stack software developer  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/5678/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/intensiveFoundations.png" width={200} />
-            <div>
-              <h5> CS5001 Intensive Foundations of CS </h5>
-              <p className="wd-dashboard-course-title">
-                Intensive Foundations of Computer Science  </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/91011/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/discreteStructures.jpg" width={200} />
-            <div>
-              <h5> CS5002 Discrete Structures </h5>
-              <p className="wd-dashboard-course-title">
-                Discrete Structures </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/121314/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/java.png" width={200} />
-            <div>
-              <h5> CS5003 Object Oriented Programming </h5>
-              <p className="wd-dashboard-course-title">
-                Object Oriented Programming </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/151617/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/dataStructures.jpg" width={200} />
-            <div>
-              <h5> CS5004 Data Structures and Algorithms </h5>
-              <p className="wd-dashboard-course-title">
-                Data Structures and Algorithms </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/181920/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/javascript.jpg" width={200} />
-            <div>
-              <h5> CS5005 Software Engineering </h5>
-              <p className="wd-dashboard-course-title">
-                Software Engineering </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/212223/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/htmlcss.jpg" width={200} />
-            <div>
-              <h5> CS5006 Web Development </h5>
-              <p className="wd-dashboard-course-title">
-                Web Development </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/242526/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/mobile.png" width={200} />
-            <div>
-              <h5> CS5007 Mobile App Development </h5>
-              <p className="wd-dashboard-course-title">
-                Mobile App Development </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/272829/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/database.png" width={200} />
-            <div>
-              <h5> CS5008 Database Systems </h5>
-              <p className="wd-dashboard-course-title">
-                Database Systems </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/303132/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/cloud.png" width={200} />
-            <div>
-              <h5> CS5009 Cloud Computing </h5>
-              <p className="wd-dashboard-course-title">
-                Cloud Computing </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/333435/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/machinelearning.png" width={200} />
-            <div>
-              <h5> CS5010 Machine Learning </h5>
-              <p className="wd-dashboard-course-title">
-                Machine Learning </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-        <div className="wd-dashboard-course">
-          <Link to="/Kambaz/Courses/363738/Home"
-            className="wd-dashboard-course-link" >
-            <img src="/images/artificialintelligence.jpeg" width={200} />
-            <div>
-              <h5> CS5011 Artificial Intelligence </h5>
-              <p className="wd-dashboard-course-title">
-                Artificial Intelligence </p>
-              <button> Go </button>
-            </div>
-          </Link>
-        </div>
-
-      </div>
-    </div>
+    <Container id="wd-dashboard">
+      <h1 id="wd-dashboard-title">Dashboard</h1>
+      <hr />
+      <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2>
+      <hr />
+      <Row xs={1} md={3} lg={4} className="g-4">
+        {courses.map((course) => (
+          <Col key={course.id} className="wd-dashboard-course">
+            <Card className='wd-course-card'>
+              <Link
+                to={`/Kambaz/Courses/${course.id}/Home`}
+                className="wd-dashboard-course-link text-decoration-none text-dark"
+              >
+                <Card.Img
+                  variant="top"
+                  src={course.img}
+                  height={160}
+                  style={{ objectFit: "cover" }}
+                />
+                <Card.Body>
+                  <Card.Title className="wd-dashboard-course-title text-nowrap overflow-hidden">
+                    {course.title}
+                  </Card.Title>
+                  <Card.Text
+                    className="wd-dashboard-course-description overflow-hidden"
+                    style={{ height: "100px" }}
+                  >
+                    {course.description}
+                  </Card.Text>
+                  <Button variant="primary">Go</Button>
+                </Card.Body>
+              </Link>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
--- src/Kambaz/Navigation.tsx
+++ src/Kambaz/Navigation.tsx
@@ -1,14 +1,60 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import "./styles.css";
+import { TiGroup } from "react-icons/ti";
+import { IoHelpCircleOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser, FaChalkboardUser } from "react-icons/fa6";
+import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function KambazNavigation() {
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br />
-      <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br />
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br />
-      <Link to="/Kambaz/Courses/1234" id="wd-course-link">Courses</Link><br />
-      <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br />
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br />
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br />
-    </div>
-  );
+    <ListGroup id="wd-kambaz-navigation"
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <ListGroup.Item id="wd-neu-link" target="_blank" action
+        href="https://www.northeastern.edu/"
+        className="bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" /></ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Account" as={Link}
+        className="text-center border-0 bg-black text-white">
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Dashboard" as={Link}
+        className="text-center border-0
+                  bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Dashboard" as={Link}
+        className="text-white
+                  bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Dashboard" as={Link}
+        className="text-white
+                  bg-black text-center border-0">
+        <TiGroup className="fs-1 text-danger" /><br />
+        Groups </ListGroup.Item><br />
+      {/* complete styling the rest of the links */}
+      <ListGroup.Item to="/Kambaz/Calendar" as={Link}
+        className="text-white bg-black text-center border-0">
+        <IoCalendarOutline className="fs-1 text-danger" /><br />
+        Calendar </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Inbox" as={Link}
+        className="text-white bg-black text-center border-0">
+        <FaInbox className="fs-1 text-danger" /><br />
+        Inbox </ListGroup.Item><br />
+      {/*Create for History Studio and Help */}
+      <ListGroup.Item to="/Kambaz/History" as={Link}
+        className="text-white bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        History </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Studio" as={Link}
+        className="text-white bg-black text-center border-0">
+        <FaChalkboardUser className="fs-1 text-danger" /><br />
+        Studio </ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Help" as={Link}
+        className="text-white bg-black text-center border-0">
+        <IoHelpCircleOutline className="fs-1 text-danger" /><br />
+        Help </ListGroup.Item><br />
+    </ListGroup>);
 }
