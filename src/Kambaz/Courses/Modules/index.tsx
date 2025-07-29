66babd93a2a09635f8324e7e7adf8f5b6851aa85
AntonMenchaca <menchacanton@gmail.com>
2025-07-28 20:05:53 -0400

    refactor module index.ts file to use reducers from redux store

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -1,5 +1,8 @@
 import { BsGripVertical } from "react-icons/bs";
 import ModulesControls from "./ModulesControls";
+import { addModule, editModule, updateModule, deleteModule }
+  from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
 import { FormControl, ListGroup } from "react-bootstrap";
 import { useParams } from "react-router";
 import * as db from "../../Database";
@@ -9,29 +12,22 @@ import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 export default function Modules() {
   const { cid } = useParams();
+  const { modules } = useSelector((state: any) => state.modulesReducer);
+  const dispatch = useDispatch();
   const [moduleName, setModuleName] = useState("");
-  const [modules, setModules] = useState<any[]>(db.modules);
-  const addModule = () => {
-    setModules([...modules, { _id: uuidv4(), name: moduleName, course: cid, lessons: [] }]);
-    setModuleName("");
-  };
-  const deleteModule = (moduleId: string) => {
-    setModules(modules.filter((m) => m._id !== moduleId));
-  };
-  const editModule = (moduleId: string) => {
-    setModules(modules.map((m) => (m._id === moduleId ? { ...m, editing: true } : m)));
-  };
-  const updateModule = (module: any) => {
-    setModules(modules.map((m) => (m._id === module._id ? module : m)));
-  };
 
 
 
   return (
     <div className="wd-modules">
-      <ModulesControls moduleName={moduleName} setModuleName={setModuleName} addModule={addModule} /><br /><br /><br /><br />
+      <ModulesControls moduleName={moduleName} setModuleName={setModuleName}
+        addModule={() => {
+          dispatch(addModule({ name: moduleName, course: cid }));
+          setModuleName("");
+        }} />
+      <br /><br /><br /><br />
       <ListGroup className="rounded-0" id="wd-modules">
-        {modules.filter((m) => m.course === cid).map((module: any) => (
+        {modules.filter((m: any) => m.course === cid).map((module: any) => (
           <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
             <div className="wd-title p-3 ps-2 bg-secondary">
 
@@ -39,18 +35,19 @@ export default function Modules() {
               {!module.editing && module.name}
               {module.editing && (
                 <FormControl className="w-50 d-inline-block"
-                  onChange={(e) => updateModule({ ...module, name: e.target.value })}
+                  onChange={(e) =>
+                    dispatch(updateModule({ ...module, name: e.target.value }))}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
-                      updateModule({ ...module, editing: false });
+                      dispatch(updateModule({ ...module, editing: false }));
                     }
                   }}
                   defaultValue={module.name} />
               )}
               <ModuleControlButtons
                 moduleId={module._id}
-                deleteModule={deleteModule}
-                editModule={editModule} />
+                deleteModule={() => dispatch(deleteModule(module._id))}
+                editModule={(moduleId) => dispatch(editModule(moduleId))} />
 
             </div>
             {module.lessons && (
