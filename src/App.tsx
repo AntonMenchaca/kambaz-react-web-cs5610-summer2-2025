68d2bb4c226e45bcd4b86e73821e46f7ccee16bd
AntonMenchaca <menchacanton@gmail.com>
2025-08-18 11:01:01 -0400

    set style for footer to be at the 10 from the bottom of the container

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,20 @@ export default function App() {
   return (
     <HashRouter>
       <Provider store={store}>
-        <div>
+        <div >
           <Routes>
             <Route path="/" element={<Navigate to="Kambaz" />} />
             <Route path="/logout" element={<Logout />} />
             <Route path="/Labs/*" element={<Labs />} />
             <Route path="/Kambaz/*" element={<Kambaz />} />
           </Routes>
-          <footer className="text-center mt-4" style={{ width: '100%', fontSize: '0.6em' }}>
-            <div>Github Frontend React Repo: https://github.com/AntonMenchaca/kambaz-react-web-cs5610-summer2-2025/tree/quizzes-project</div>
-            <div>Github Node Repo: https://github.com/AntonMenchaca/kambaz-node-server-app</div>
-            <p>Anton Menchaca CS5610 2025 - Summer 2</p>
-          </footer>
+
         </div>
+        <footer className="text-center mt-4" style={{ width: '100%', fontSize: '0.6em', bottom: 10 }}>
+          <div>Github Frontend React Repo: https://github.com/AntonMenchaca/kambaz-react-web-cs5610-summer2-2025/tree/quizzes-project</div>
+          <div>Github Node Repo: https://github.com/AntonMenchaca/kambaz-node-server-app</div>
+          <p>Anton Menchaca CS5610 2025 - Summer 2</p>
+        </footer>
       </Provider>
     </HashRouter>);
 }
